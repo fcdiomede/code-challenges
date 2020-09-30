ffaba2b0b8e47c2f5ba8611d1353c66560f765c6
Fernanda Diomede <fcdiomede@gmail.com>
2020-09-30 12:04:40 -0500

    recursion challeges

--- recursion.py
+++ recursion.py
@@ -8,6 +8,24 @@ def print_recursively(item_list):
        print(item_list[0])
        print_recursively(item_list[1:])
 
-print_recursively(["apple", "berry", "cherry"])
-print_recursively(["apple"])
-print_recursively([])
+# print_recursively(["apple", "berry", "cherry"])
+# print_recursively(["apple"])
+# print_recursively([])
+
+#Write a recursive function that prints the numbers 1 through 5.
+
+def print_1_through_5_recursively(num=1):
+    #base case is number at 5
+    #with a loop:
+    # num = 1
+    # while num < 5:
+    #     print(num)
+    #     num += 1
+    
+    if num > 5:
+        return
+    
+    print(num)
+    print_1_through_5_recursively(num+1)
+
+print_1_through_5_recursively()
